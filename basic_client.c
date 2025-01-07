6b8e56485f255b01420fcdf7472230ac28708b65
Steven Luo <stevenl350@nycstudents.net>
2025-01-06 19:40:05 -0500

    Sending message works? Except CTRL-C doesn't end the server when there's a client

--- basic_client.c
+++ basic_client.c
@@ -1,41 +1,33 @@
 #include "pipe_networking.h"
 
 int main() {
-
   int to_server;
   int from_server;
 
   from_server = client_handshake( &to_server );
 
-  // char buffer[BUFFER_SIZE];
-  // int fd = open("/dev/urandom", O_RDONLY);
-  // int num;
-  // int bytes;
-  // while(1) {
-  //   bytes = read(from_server, buffer, BUFFER_SIZE);
-  //   if (bytes < 0) {
-	//   	perror("read failed");
-	//   	exit(1);
-	//   }
-	//   printf("Receiving %s from server\n", buffer);
-  //
- 	//   bytes = read(fd, &num, sizeof(int));
- 	//   if (bytes < 0) {
- 	//   	perror("read failed");
- 	//   	exit(1);
- 	//   }
- 	//   printf("Sending %d to server\n", num);
-  //
-	// 	snprintf(buffer, BUFFER_SIZE, "%d", num);
- 	//   bytes = write(to_server, buffer, BUFFER_SIZE);
-  //   if (bytes == 0) {
-  //     printf("Client stopped connection\n");
-  //     exit(0);
-  //   }
- 	//   if (bytes < 0) {
- 	//   	perror("write failed");
- 	//   	exit(1);
- 	//   }
- 	//   sleep(1);
-  // }
+  char buffer[BUFFER_SIZE];
+  int num = getpid();
+  int bytes;
+  while(1) {
+    bytes = read(from_server, buffer, BUFFER_SIZE);
+    if (bytes < 0) {
+	  	perror("read failed");
+	  	exit(1);
+	  }
+    printf("Received message %s from server\n", buffer);
+
+		snprintf(buffer, BUFFER_SIZE, "%d", num);
+ 	  bytes = write(to_server, buffer, BUFFER_SIZE);
+    if (bytes == 0) {
+      printf("Server stopped connection\n");
+      exit(0);
+    }
+ 	  if (bytes < 0) {
+ 	  	perror("write failed");
+ 	  	exit(1);
+ 	  }
+    printf("Sent pid %d to server\n", num);
+ 	  sleep(1);
+  }
 }
--- forking_server.c
+++ forking_server.c
@@ -1,5 +1,33 @@
 #include "pipe_networking.h"
 
+void subserver(int from_client, int to_client) {
+  char message[BUFFER_SIZE];
+  snprintf(message, BUFFER_SIZE, "INTERCONNECTIVITY");
+
+  char buffer[BUFFER_SIZE];
+  int bytes;
+  while(1) {
+    bytes = write(to_client, message, BUFFER_SIZE);
+    if (bytes == 0) {
+      printf("Client stopped connection\n");
+      exit(0);
+    }
+ 	  if (bytes < 0) {
+ 	  	perror("write failed");
+ 	  	exit(1);
+ 	  }
+    printf("Sent message %s to client\n", message);
+
+    bytes = read(from_client, buffer, BUFFER_SIZE);
+    if (bytes < 0) {
+	  	perror("read failed");
+	  	exit(1);
+	  }
+    printf("Received pid %s from client\n", buffer);
+ 	  sleep(1);
+  }
+}
+
 int main() {
   int to_client;
   int from_client;
@@ -9,6 +37,8 @@ int main() {
     f = fork();
     if(f == 0) {
       server_handshake_half(&to_client, from_client);
+      subserver(from_client, to_client);
+      exit(0);
     }
     else {
       close(from_client);
